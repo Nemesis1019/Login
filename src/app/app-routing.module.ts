6ab0af1e4ccdadab7d310b55a52915d49f4124ad
caac1019 <caac1019@gmail.com>
2024-09-02 06:47:46 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/components/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: FormComponent }, // Configura la ruta para el módulo form
+  { path: '**', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
